d10ebc0d3696439b701479fb81fabc5d5ffc3bf7
liushuyu <liushuyu011@gmail.com>
2018-09-24 22:27:39 -0600

    web: add basic table structure

--- pakreq/routes.py
+++ pakreq/routes.py
@@ -2,13 +2,15 @@
 
 import pathlib
 
-from pakreq.views import index, requests_json
+from pakreq.views import index, requests_all, request_detail
 
 PROJECT_ROOT = pathlib.Path(__file__).parent
 
+
 def setup_routes(app):
     app.router.add_get('/', index)
-    app.router.add_get('/requests.json', requests_json)
+    app.router.add_get('/requests', requests_all)
+    app.router.add_get('/request/{ids:([0-9]*)}', request_detail)
     setup_static_routes(app)
 
 
--- pakreq/views.py
+++ pakreq/views.py
@@ -9,13 +9,22 @@ import pakreq.db
 
 from pakreq.utils import dump_json
 
+
 @aiohttp_jinja2.template('index.html')
 async def index(request):
     async with request.app['db'].acquire() as conn:
         requests = await pakreq.db.get_requests(conn)
     return {'requests': requests}
 
-async def requests_json(request):
+
+async def requests_all(request):
     async with request.app['db'].acquire() as conn:
         result = await pakreq.db.get_requests(conn)
+    return web.json_response(result, dumps=dump_json)
+
+
+async def request_detail(request):
+    ids = request.match_info['ids']
+    async with request.app['db'].acquire() as conn:
+        result = await pakreq.db.get_request_detail(conn, ids)
     return web.json_response(result, dumps=dump_json)
