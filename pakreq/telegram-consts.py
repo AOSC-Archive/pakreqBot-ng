088178ebf1eaf96f24361d54301ce3b5ff4fa0dd
liushuyu <liushuyu011@gmail.com>
2018-09-27 21:52:32 -0600

    add missing file(s)

---
+++ pakreq/telegram-consts.py
@@ -0,0 +1,9 @@
+REQUEST_DETAIL = """
+<b>{name}:</b>
+<b>Type:</b> {type}
+<b>Description:</b> {desc}
+<b>Requester:</b> {req_name}({req_id})
+<b>Packager:</b> {pak_name}({pak_id})
+<b>Created on:</b> {date}
+<b>ETA:<b> {eta}
+"""
