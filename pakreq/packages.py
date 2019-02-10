51a053f693a052ab1040d96e703d85bbfd76f0e7
Kay Lin <kaymw@aosc.io>
2019-02-09 21:34:33 -0800

    packages: Initiative
    Gonna use the packages site to check validity of requests.
    As well as closing requests automatically.
    
    Signed-off-by: Kay Lin <kaymw@aosc.io>

---
+++ pakreq/packages.py
@@ -0,0 +1,24 @@
+# packages.py
+
+from aiohttp import ClientSession
+
+
+BASE_URL='https://packages.aosc.io'
+
+
+async def make_request(url, params={}):
+    session = ClientSession()
+    if not params['type']:
+        params['type'] = 'json'
+    async with session.get(url, params=params) as resp:
+        result = await resp.json()
+    await session.close()
+    return result
+
+
+async def search_packages(name):
+    return make_request('%s/search/' % BASE_URL, {'q': name})
+
+
+async def get_package_info(name):
+    return make_request('%s/packages/%s' % (BASE_URL, name))
